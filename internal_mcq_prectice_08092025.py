4f1049eab51cb419f5585fac23d9cb9730799183
Ayandiwan <ayandiwan8769@gmail.com>
2025-09-09 18:18:58 +0530

    Add files via upload

---
+++ internal_mcq_prectice_08092025.py
@@ -0,0 +1,146 @@
+print("Loading...",flush=True)
+
+Samosa = 3.25
+print(f"Samosa: ₹{Samosa:.2f}")
+
+tup=("ad","king"),("ad","king")
+print(tup)
+
+
+print(3==3) #True
+print('3'==3)  #False
+
+
+list=[1,2,3,4,5]
+list.append(5)
+list[0]
+print(list)
+
+
+age=19
+
+print("done") if age > 20 else print("sorry")
+
+print("AD".lower())
+
+print(len(["hello",2,4,6]))
+
+
+t1=(1,2,3,4,5)
+t1[0]=6
+
+
+x='abcd'
+for i in range(len(x)):
+    print(i)
+    
+tup=(4,5,6)
+tup[1]    
+
+ad="aaa"
+ad.count('a')
+
+
+print(4+3%5)
+
+
+x='Anand Agricultural'
+print(x.find('ri'))
+
+
+s='Ayan'
+print(s[-1])
+
+
+
+
+
+
+import array
+arr = array.array('i', [1, 2, 3, 4, 5])
+print(arr[2])  
+arr.append(6)
+arr.remove(2)
+print(arr)  # Output: array('i', [1, 3, 4, 5, 6])
+
+
+
+my_list = [1, 2, 3]
+my_list[1:3]
+
+my_list.insert(1, 10)
+my_list
+
+del my_list[0]
+
+
+my_tuple = (1, 2, 3,4,5)
+my_tuple[:3]
+
+
+print(chr(ord('B')))
+
+
+print(round(4.276))
+
+all([3,4,0,7])
+
+print(2**2**3**1)
+
+
+str1="javat"
+str2=":"
+str3="point"
+print(str1[-1:])
+print(r"\njavat\npoint")
+
+list=['a','b','c']
+list+='de'
+
+list
+
+l=['a','b','c']
+print("".join(l))
+
+
+lit=[1,2,3,4,]
+
+print(2**3**4**2)
+
+list=[1,2,4,5]
+print(list[1])
+
+
+print(chr(ord(chr(66))))
+
+print(2**(3**2))
+
+
+ex="snow world"
+print("%s" %ex[4:7])
+
+max("what are you")
+
+
+
+ex="hello"
+print(ex.count("l"))
+
+t1=(1,2,3,4)
+t1.append((5,6,7))
+t1
+
+print(len(t1))
+print(3*1**3)
+
+
+y=[2,15,6]
+y.sort()
+y
+
+
+print(i.lower() for i in "AIT312")
+
+L=[2,3,4]
+L2=[5,6,8]
+print(len(L+L2))
