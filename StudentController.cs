d8c3125b34359f251644e0f27f8f670662f60c5f
Ayandiwan <ayandiwan8769@gmail.com>
2025-12-03 19:49:24 +0530

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using p_28_EF.Models;
+
+namespace p_28_EF.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Student Data Submitted Successfully!";
+                return View("Success");
+            }
+
+            return View(s);
+        }
+
+
+
+    }
+}
